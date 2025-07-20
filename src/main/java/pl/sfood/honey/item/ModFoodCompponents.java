3e1db848c8566e88a12c01e5b3a2a0f98a343dec
Ryjek <patrolib@gmail.com>
2025-07-20 10:54:08 +0200

    add new foods

--- src/main/java/pl/sfood/honey/item/ModFoodCompponents.java
+++ src/main/java/pl/sfood/honey/item/ModFoodCompponents.java
@@ -1,8 +1,33 @@
 package pl.sfood.honey.item;
 
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 
 public class ModFoodCompponents {
-    public static final FoodComponent HOT_DOG = new FoodComponent.Builder().nutrition(5).saturationModifier(0.25f).build();
-
+    public static final FoodComponent HOT_DOG = new FoodComponent.Builder().nutrition(7).saturationModifier(0.25f).build();
+    public static final FoodComponent ICE_CREAM_CHOCOLATE = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent ICE_CREAM_STRAWBERRY = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent ICE_CREAM_SALTED_CARMEL = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent ICE_CREAM_VANILLA = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent BANANA_SHAKE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent BLUEBERRY_SHAKE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent CARMEL_SHAKE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent CHOCOLATE_SHAKE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent STRAWBERRY_SHAKE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent VANILLA_SHAKE = new FoodComponent.Builder().nutrition(5).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
+    public static final FoodComponent TEPICOLA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.25f).build();
+    public static final FoodComponent OSHAWATA = new FoodComponent.Builder().nutrition(4).saturationModifier(0.25f).build();
+    public static final FoodComponent BOMBARDILOTOTODILO = new FoodComponent.Builder().nutrition(4).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150), 0.15f).build();
 }
--- src/main/java/pl/sfood/honey/item/ModItems.java
+++ src/main/java/pl/sfood/honey/item/ModItems.java
@@ -10,11 +10,19 @@ import pl.sfood.honey.SimpleFoods;
 
 public class ModItems {
     public static final Item HOT_DOG = registerItem("hot_dog", new Item(new Item.Settings().food(ModFoodCompponents.HOT_DOG)));
-    public static final Item ICE_CREAM_CHOCOLATE = registerItem("ice_cream_chocolate", new Item(new Item.Settings()));
-    public static final Item ICE_CREAM_SALTED_CARMEL = registerItem("ice_cream_salted_carmel", new Item(new Item.Settings()));
-    public static final Item ICE_CREAM_STRAWBERRY = registerItem("ice_cream_strawberry", new Item(new Item.Settings()));
-    public static final Item ICE_CREAM_VANILLA = registerItem("ice_cream_vanilla", new Item(new Item.Settings()));
-
+    public static final Item ICE_CREAM_CHOCOLATE = registerItem("ice_cream_chocolate", new Item(new Item.Settings().food(ModFoodCompponents.ICE_CREAM_CHOCOLATE)));
+    public static final Item ICE_CREAM_SALTED_CARMEL = registerItem("ice_cream_salted_carmel", new Item(new Item.Settings().food(ModFoodCompponents.ICE_CREAM_SALTED_CARMEL)));
+    public static final Item ICE_CREAM_STRAWBERRY = registerItem("ice_cream_strawberry", new Item(new Item.Settings().food(ModFoodCompponents.ICE_CREAM_STRAWBERRY)));
+    public static final Item ICE_CREAM_VANILLA = registerItem("ice_cream_vanilla", new Item(new Item.Settings().food(ModFoodCompponents.ICE_CREAM_VANILLA)));
+    public static final Item BANANA_SHAKE = registerItem("banana_shake", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.BANANA_SHAKE)));
+    public static final Item BLUEBERRY_SHAKE = registerItem("blueberry_shake", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.BLUEBERRY_SHAKE)));
+    public static final Item CARMEL_SHAKE = registerItem("carmel_shake", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.CARMEL_SHAKE)));
+    public static final Item CHOCOLATE_SHAKE = registerItem("chocolate_shake", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.CHOCOLATE_SHAKE)));
+    public static final Item STRAWBERRY_SHAKE = registerItem("strawberry_shake", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.STRAWBERRY_SHAKE)));
+    public static final Item VANILLA_SHAKE = registerItem("vanilla_shake", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.VANILLA_SHAKE)));
+    public static final Item TEPICOLA = registerItem("tepicola", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.TEPICOLA)));
+    public static final Item OSHAWATA = registerItem("oshawata", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.OSHAWATA)));
+    public static final Item BOMBARDILOTOTODILO = registerItem("bombardilototodilo", new DrinkableItem(new Item.Settings().food(ModFoodCompponents.BOMBARDILOTOTODILO)));
 
 
 
@@ -32,6 +40,15 @@ public class ModItems {
             entries.add(ICE_CREAM_STRAWBERRY);
             entries.add(ICE_CREAM_SALTED_CARMEL);
             entries.add(ICE_CREAM_VANILLA);
+            entries.add(BANANA_SHAKE);
+            entries.add(BLUEBERRY_SHAKE);
+            entries.add(CARMEL_SHAKE);
+            entries.add(CHOCOLATE_SHAKE);
+            entries.add(STRAWBERRY_SHAKE);
+            entries.add(VANILLA_SHAKE);
+            entries.add(TEPICOLA);
+            entries.add(OSHAWATA);
+            entries.add(BOMBARDILOTOTODILO);
 
         });
     }
